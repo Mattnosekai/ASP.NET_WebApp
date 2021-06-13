7f7de727e03367f8056a5851e3b0da8024c89c7c
Mattnosekai <theprogramman@yahoo.com>
2021-06-13 16:58:36 -0500

    First commit

---
+++ Contact.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="WebApp3.Contact" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Matt's Web App Database Example 1</title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 121px;
+        }
+    </style>
+</head>
+<body>
+    <center>
+    <img src="Mattnosekai_logo1.png" alt="Mattnosekai Logo" width="460" height="460">
+    <form id="form1" runat="server">
+        <div>
+            <table>
+            <tr>  
+            <td>
+                <asp:Label ID="lblContactID" runat="server" Text="ContactID"></asp:Label>
+            </td>
+            <td colspan="2" class="auto-style1">
+                <asp:TextBox ID="txtContactID" runat="server" Width="157px"></asp:TextBox>
+             </td>
+             </tr>  
+             <tr>   
+             <td>
+                <asp:Label ID="lblName" runat="server" Text="Name"></asp:Label>
+             </td> 
+             <td colspan="2" class="auto-style1">
+                <asp:TextBox ID="txtName" runat="server" Width="158px"></asp:TextBox>
+            </td>
+            </tr>
+            
+            <tr>  
+            <td>
+                <asp:Label ID="lblMobile" runat="server" Text="Phone"></asp:Label>
+            </td>
+            <td colspan="2" class="auto-style1">
+                <asp:TextBox ID="txtMobile" runat="server" Width="158px"></asp:TextBox>
+             </td>
+             </tr>    
+
+             <tr>  
+            <td>
+                <asp:Label ID="lblAddress" runat="server" Text="Address"></asp:Label>
+            </td>
+            <td colspan="2" class="auto-style1">
+                <asp:TextBox ID="txtAddress" runat="server" Width="158px"></asp:TextBox>
+             </td>
+             </tr>   
+
+
+            </table>
+            </div>
+             
+            <asp:Button ID="Button1" runat="server" Text="Query/Look-up" OnClick="Button1_Click" Width="103px" />
+        
+            <asp:Button ID="Button2" runat="server" Text="Add/Update" OnClick="Button2_Click" Width="92px" />
+        
+            <asp:Button ID="Button3" runat="server" Text="Delete" OnClick="Button3_Click" Width="92px" /> 
+
+            <asp:Button ID="Button4" runat="server" Text="Clear" OnClick="Button4_Click" Width="92px" />
+        <br />
+            <asp:Label ID="lblResponse" runat="server" Text=""></asp:Label>
+        
+    </form>
+</center>
+</body>
+</html>
---
+++ Contact.aspx.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApp3
+{
+    public partial class Contact : System.Web.UI.Page
+    {
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=(local)\sqlexpress;Initial Catalog=ASPWP;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            lblResponse.ForeColor = System.Drawing.Color.DarkGray;
+            lblResponse.Font.Bold = false;
+            lblResponse.Text = "Please wait...";
+            //txtName.Text = "";
+            //txtMobile.Text = "";
+            //txtAddress.Text = "";
+
+
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+
+            string sqlcmd1 = "SELECT * FROM Contact WHERE ContactID = " + txtContactID.Text;
+
+            SqlCommand sqlCmd = new SqlCommand(sqlcmd1, sqlCon);
+
+            sqlCmd.ExecuteNonQuery();
+
+            SqlDataReader reader = sqlCmd.ExecuteReader();
+
+            reader.Read();
+
+
+            if (reader.HasRows)
+            {
+                txtName.Text = reader["Name"].ToString();
+                txtMobile.Text = reader["Mobile"].ToString();
+                txtAddress.Text = reader["Address"].ToString();
+
+                lblResponse.ForeColor = System.Drawing.Color.Green;
+                lblResponse.Font.Bold = true;
+                lblResponse.Text = "Record Exists";
+            }
+            else
+            {
+                txtName.Text = "";
+                txtMobile.Text = "";
+                txtAddress.Text = "";
+                lblResponse.ForeColor = System.Drawing.Color.Red;
+                lblResponse.Font.Bold = true;
+                lblResponse.Text = "Record Not Found: Does Not Exist";
+            }
+
+
+            reader.Close();
+            reader.Dispose();
+
+
+            sqlCon.Close();
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            lblResponse.ForeColor = System.Drawing.Color.DarkGray;
+            lblResponse.Font.Bold = false;
+            lblResponse.Text = "Please wait...";
+           
+
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+
+            string sqlCmd1 = "SELECT * FROM Contact WHERE ContactID = " + txtContactID.Text;
+
+            SqlCommand sqlCmd = new SqlCommand(sqlCmd1, sqlCon);
+
+            sqlCmd.ExecuteNonQuery();
+
+            SqlDataReader reader = sqlCmd.ExecuteReader();
+
+            reader.Read();
+
+            bool hasData = reader.HasRows;
+            reader.Close();
+            reader.Dispose();
+
+            if (hasData)
+            {
+
+                SqlCommand sqlCmd2 = new SqlCommand("UPDATE Contact SET Name='" + txtName.Text + "', Mobile='" + txtMobile.Text + "', Address='" + txtAddress.Text + "' WHERE ContactID=" + txtContactID.Text, sqlCon);
+
+                sqlCmd2.ExecuteNonQuery();
+
+                lblResponse.ForeColor = System.Drawing.Color.Green;
+                lblResponse.Font.Bold = true;
+                lblResponse.Text = "Record Updated";
+            }
+            else
+            {
+
+                SqlCommand sqlCmd3 = new SqlCommand("INSERT INTO Contact (ContactID, Name, Mobile, Address) VALUES('" + txtContactID.Text + "'," + "'" + txtName.Text + "','" + txtMobile.Text + "','" + txtAddress.Text + "')", sqlCon);
+
+                sqlCmd3.ExecuteNonQuery();
+
+                lblResponse.ForeColor = System.Drawing.Color.Green;
+                lblResponse.Font.Bold = true;
+                lblResponse.Text = "New Record Added";
+            }
+
+
+            sqlCon.Close();
+
+
+
+        }
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            lblResponse.ForeColor = System.Drawing.Color.DarkGray;
+            lblResponse.Font.Bold = false;
+            lblResponse.Text = "Please wait...";
+           
+
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+
+            string sqlCmd1 = "SELECT * FROM Contact WHERE ContactID = " + txtContactID.Text;
+
+            SqlCommand sqlCmd = new SqlCommand(sqlCmd1, sqlCon);
+
+            sqlCmd.ExecuteNonQuery();
+
+            SqlDataReader reader = sqlCmd.ExecuteReader();
+
+            reader.Read();
+
+            bool hasData = reader.HasRows;
+            reader.Close();
+            reader.Dispose();
+
+            if (hasData)
+            {
+                SqlCommand sqlCmd2 = new SqlCommand("DELETE FROM Contact WHERE ContactID='" + txtContactID.Text + "'", sqlCon);
+
+                sqlCmd2.ExecuteNonQuery();
+
+                lblResponse.ForeColor = System.Drawing.Color.Green;
+                lblResponse.Font.Bold = true;
+                lblResponse.Text = "Record Deleted";
+            }
+            else
+            {
+                lblResponse.ForeColor = System.Drawing.Color.Red;
+                lblResponse.Font.Bold = true;
+                lblResponse.Text = "Record Not Found: Does Not Exist";
+            }
+            sqlCon.Close();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            txtContactID.Text = "";
+            txtName.Text = "";
+            txtMobile.Text = "";
+            txtAddress.Text = "";
+            lblResponse.Text = "";
+
+        }
+    }
+}
+
